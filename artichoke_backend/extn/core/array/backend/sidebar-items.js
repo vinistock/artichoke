297e57ae6103f8725f7cb6876ca6d6b2bfc72751
ci-build <ci-build@hyperbo.la>
2019-11-24 01:49:26 +0000

    [skip ci] build docs

--- artichoke_backend/extn/core/array/backend/sidebar-items.js
+++ artichoke_backend/extn/core/array/backend/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["aggregate",""],["buffer",""],["fixed",""],["integer_range",""],["repeated",""]]});
+initSidebarItems({"mod":[["aggregate",""],["buffer",""],["fixed",""],["integer_range",""],["repeated",""]],"trait":[["ArrayType",""]]});
--- implementors/downcast/trait.Downcast.js
+++ implementors/downcast/trait.Downcast.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["artichoke_backend"] = [{text:"impl&lt;_T:&nbsp;Any&gt; Downcast&lt;_T&gt; for dyn <a class=\"trait\" href=\"artichoke_backend/extn/core/array/trait.ArrayType.html\" title=\"trait artichoke_backend::extn::core::array::ArrayType\">ArrayType</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;_T: Any,&nbsp;</span>",synthetic:false,types:["artichoke_backend::extn::core::array::ArrayType"]},];
+implementors["artichoke_backend"] = [{text:"impl&lt;_T:&nbsp;Any&gt; Downcast&lt;_T&gt; for dyn <a class=\"trait\" href=\"artichoke_backend/extn/core/array/backend/trait.ArrayType.html\" title=\"trait artichoke_backend::extn::core::array::backend::ArrayType\">ArrayType</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;_T: Any,&nbsp;</span>",synthetic:false,types:["artichoke_backend::extn::core::array::backend::ArrayType"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
